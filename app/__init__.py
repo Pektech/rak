fd648a96e776cc8ad5532cbda4f05f71bd3d48f3
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-11-10 22:24:19 -0500

    basic setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,23 @@
- 
+from flask import Flask
+from flask_ask import Ask
+
+app = Flask(__name__)
+ask = Ask(app, "/")
+
+import logging
+
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+
+# from app import alexa
+
+# from app import mechanics
+
+
+@app.route("/pek")
+def hello_world():
+    return "Hello Pek and Monk"
+
+
+if __name__ == "__main__":
+    app.run()
